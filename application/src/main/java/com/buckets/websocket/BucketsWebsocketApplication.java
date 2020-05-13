2b4d0f1f77c71798b32b5e1eb76f259af29a4373
oujiayong <oujiayong@do1.com.cn>
2020-05-13 15:09:43 +0800

    init develop

---
+++ application/src/main/java/com/buckets/websocket/BucketsWebsocketApplication.java
@@ -0,0 +1,20 @@
+package com.buckets.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ChenCaiJun on 2019/08/02
+ *
+ * @author tangxt
+ * @version 1.0
+ * @since 1.8
+ */
+@SpringBootApplication
+public class BucketsWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BucketsWebsocketApplication.class, args);
+    }
+
+}
